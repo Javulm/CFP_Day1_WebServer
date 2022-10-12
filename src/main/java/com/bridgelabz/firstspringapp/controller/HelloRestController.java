c89b07144c041a40b06a9936f516fa13be5c54a9
Javul Mulla <javulmulla8@gmail.com>
2022-10-12 10:36:17 +0530

    Added:Respond hello messages to the User.

---
+++ src/main/java/com/bridgelabz/firstspringapp/controller/HelloRestController.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.firstspringapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloRestController {
+    @GetMapping("/message")
+    public String message() {
+        return "Hello From BridgeLabz";
+    }
+}
